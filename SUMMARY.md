a23f615b838b61bb3e9ecc7eea4b7838d6b7f186
Ars Vladimirov <grokhael@gmail.com>
2023-07-21 12:02:27 +0600

    upd custom data

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,7 +1,7 @@
-**Cracks and Potholes in Road Images** is a dataset for semantic segmentation and instance segmentation tasks. It is used in the industrial industry.
+**Cracks and Potholes in Road Images** is a dataset for semantic segmentation and instance segmentation tasks. It is used in the utilities and safety industries. 
 
 The dataset consists of 2235 images with 4720 labeled objects belonging to 3 different classes including *road*, *cracks*, and *pothole*.
 
-Images in the Cracks and Potholes in Road dataset has pixel-level semantic segmentation annotations. Due to the nature of the semantic segmentation task, it can be automatically transformed into object detection (bounding boxes for every object) task. All images are labeled (i.e. with annotations). There is 1 split in the dataset: *ds0* (2235 images). The dataset was released in 2020.
+Images in the Cracks and Potholes in Road dataset have pixel-level semantic segmentation annotations. Due to the nature of the semantic segmentation task, it can be automatically transformed into an object detection (bounding boxes for every object) task. All images are labeled (i.e. with annotations). There are no pre-defined <i>train/val/test</i> splits in the dataset. The dataset was released in 2020 by the [Universidade do Vale do Itajai, Brazil](https://www.univali.br/Paginas/default.aspx) and [DNIT, Brazil](https://www.gov.br/dnit/pt-br).
 
 <img src="https://github.com/dataset-ninja/cracks-and-potholes-in-road/raw/main/visualizations/poster.png">
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,14 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
@@ -13,10 +21,15 @@ PROJECT_NAME_FULL = "Cracks and Potholes in Road Images"
 ##################################
 LICENSE: License = License.CC_BY_4_0()
 INDUSTRIES: List[Industry] = [Industry.Industrial()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Utilities(), Industry.Safety()]
+CATEGORY: Category = Category.EnergyAndUtilities()
+
 CV_TASKS: List[CVTask] = [CVTask.SemanticSegmentation(), CVTask.InstanceSegmentation()]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.SemanticSegmentation()]
 
-RELEASE_YEAR: int = 2020
+RELEASE_DATE: Optional[str] = "2020-07-21"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
 HOMEPAGE_URL: str = "https://data.mendeley.com/datasets/t576ydh9v8/4"
 
 PREVIEW_IMAGE_ID: int = 186409
@@ -33,10 +46,27 @@ DOWNLOAD_ORIGINAL_URL: Optional[
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
+PAPER: Optional[str] = "https://biankatpas.github.io/Cracks-and-Potholes-in-Road-Images-Dataset/"
 CITATION_URL: Optional[str] = "http://dx.doi.org/10.17632/t576ydh9v8.4"
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+AUTHORS: Optional[List[str]] = [
+    "Bianka T. Passos",
+    "Mateus J. Cassaniga",
+    "Anita M. R. Fernandes",
+    "Kátya B. Medeiros",
+    "Eros Comunello",
+]
+
+
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "Universidade do Vale do Itajai, Brazil",
+    "DNIT, Brazil",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://www.univali.br/Paginas/default.aspx",
+    "https://www.gov.br/dnit/pt-br",
+]
+
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -51,11 +81,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
-        "project_name_full": PROJECT_NAME_FULL,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -63,15 +97,20 @@ def get_settings():
         "preview_image_id": PREVIEW_IMAGE_ID,
         "github_url": GITHUB_URL,
     }
+
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
+    settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
